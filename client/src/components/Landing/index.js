085728be085848a0643907b1594aac0c4b739686
Harry Li <harryli@Iriss-iMac.local>
2019-11-02 18:18:30 -0400

    post firebase id token to server

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -11,10 +11,29 @@ class Landing extends Component {
     };
   }
 
-  get = async (url) => {
+  post = async (url) => {
     try {
-      const response = await fetch(url);
-      let data = await response.json();
+      console.log(this.props);
+      const idToken = await this.props.firebase.auth.currentUser.getIdToken(true);
+      console.log(idToken);
+      const body = {
+        firebaseIdToken: idToken
+      }
+
+      const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(body) // body data type must match "Content-Type" header
+      });
+      const data = await response.json();
       console.log("awaited response", data);
       this.setState({result: data});
     }
@@ -28,9 +47,18 @@ class Landing extends Component {
     return (
       <div>
         <div>
-          <button onClick={e => this.get("http://localhost:5000/")}>Send GET request to server "/"</button>
-          {this.state.result ? "Result: "+this.state.result : ""}
+          <button onClick={e => this.post("http://localhost:5000/")}>Send GET request to server "/"</button>
         </div>
+
+        <br/>
+
+        <div>
+          <button onClick={e => this.post("http://localhost:5000/loggedIn")}>Send GET request to server "/loggedIn"</button>
+        </div>
+
+        <br/>
+
+        {this.state.result ? "Result: "+this.state.result : ""}
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 const port = 5000
 
-app.get('/', (req, res) => {
-  console.log("received request");
+app.post('/', (req, res) => {
+  console.log("/ request received", req.body.firebaseIdToken);
   res.send(JSON.stringify('Anyone can access this data'))
 })
 
-app.get('/loggedIn', (req, res) => res.send('Only logged in users can access this data'))
+app.post('/loggedIn', (req, res) => {
+  console.log("/loggedIn request received", req.body.firebaseIdToken);
+  res.send(JSON.stringify('Only logged in users can access this data'))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
